4974fefee5b1b103fe6223c0f491cde9b5ce9049
roshnashameer <roshnasharaf123@gmail.com>
2024-05-02 10:15:44 +0530

    third_commit

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-// const connectString = process.env.MONGODB_CONNECT_URI
-// mongoose.connect(connectString).then(() => {
-//     console.log("___MongoDB Atls Connected");
-// }).catch((err) => {
-//     console.log(`____MongoDB Atlas Failed____${err}`);
-// })
+const connectString = process.env.MONGODB_CONNECT_URI
+mongoose.connect(connectString).then(() => {
+    console.log("___MongoDB Atls Connected");
+}).catch((err) => {
+    console.log(`____MongoDB Atlas Failed____${err}`);
+})
 
-mongoose.connect('mongodb://127.0.0.1:27017/movieBooking').then(() => {
-        console.log("___MongoDB Compass Connected");
-    }).catch((err) => {
-        console.log(`____MongoDB Compass Failed____${err}`);
-    })
+// mongoose.connect('mongodb://127.0.0.1:27017/movieBooking').then(() => {
+//         console.log("___MongoDB Compass Connected");
+//     }).catch((err) => {
+//         console.log(`____MongoDB Compass Failed____${err}`);
+//     })
 
